f9d65c3a5a77794e141c8911217be2db090f0f7c
Shirley Lin <r13922159@ntu.edu.tw>
2025-03-25 00:55:43 +0800

    try

--- student_agent.py
+++ student_agent.py
@@ -11,8 +11,10 @@ def get_action(obs):
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
-
-
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
-    # You can submit this random agent to evaluate the performance of a purely random strategy.
-
+    with open("q_table.pkl", "rb") as f:
+        q_table = pickle.load(f)
+    
+    if obs in q_table:
+        return int(np.argmax(q_table[obs]))
+    else:
+        return random.choice([0,1,2,3,4,5])
